f24cee7641199ca7358d355a5c600fda7db2b88a
KinRyuu <50115571+KinRyuuu@users.noreply.github.com>
2019-07-27 22:22:09 +0100

    abstracted log cleanup to separate client

--- src/runDiscord.py
+++ src/runDiscord.py
@@ -1,6 +1,5 @@
 # Run the bot as a discord client
 import random
-import datetime
 import discord
 import os
 import evebot
@@ -202,15 +201,6 @@ class DiscordClient(discord.Client):
         except discord.Forbidden as e:
             logger.error("Do not have permissions to log edited message. " + str(e))
 
-async def log_cleanup():
-        await client.wait_until_ready()
-        channel = client.get_channel(config.log_channels.get(354565059675947009))
-        while(True):
-            async for messages in channel.history(limit=None):
-                if(datetime.datetime.now().timestamp() - messages.created_at.timestamp() > 86400):
-                    await messages.delete()
-            time.sleep(86400)
-
 def hasApprovedRole(discordUser):
     for role in discordUser.roles:
         if role.id in config.approved_roles.get(discordUser.guild.id, []):
@@ -220,5 +210,4 @@ def hasApprovedRole(discordUser):
 
 if (__name__ == "__main__"):
     client = DiscordClient()
-    client.loop.create_task(log_cleanup())
     client.run(apikeys.discordkey)
---
+++ src/runLogCleanup.py
@@ -0,0 +1,49 @@
+import datetime
+import discord
+import apikeys
+import logging
+import config 
+import time
+
+logger = logging.getLogger(__name__)
+
+# Only log debug messages in debug mode
+if (config.DEBUG):
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
+
+class CleanupClient(discord.Client):
+    async def on_ready(self):
+        logger.info("Initiating log cleanup.")
+        messages = 0
+        channel = self.get_channel(config.log_channels.get(354565059675947009))
+        if(channel is None):
+            logger.error("Unable to find log channel!")
+        
+        message_list = []
+
+        async for message in channel.history(limit=None):
+            if(datetime.datetime.now().timestamp() - message.created_at.timestamp() > 86400):
+                message_list.append(message)
+                messages += 1
+
+            if len(message_list) == 100:
+                try:
+                    await self.delete_messages(message_list)
+                    time.sleep(1)
+                except:
+                    for message in message_list:
+                        await message.delete()
+                        time.sleep(1)
+
+        for message in message_list:
+            await message.delete()
+            time.sleep(1)
+
+        logger.info(str(messages) + " messages deleted.")
+        self.close()
+
+if(__name__ == "__main__"):
+    client = CleanupClient()
+    client.run(apikeys.discordkey)
